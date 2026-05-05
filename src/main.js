3ee53abb031e06e6e8df7bad3884c1dae1af3b8f
Irina Varushkina <rnvr@mail.ru>
2026-05-05 23:19:25 +0500

    Шаги 2, 3 объявляет функции calculateBonusByProfit и calculateSimpleRevenue

--- src/main.js
+++ src/main.js
@@ -5,7 +5,15 @@
  * @returns {number}
  */
 function calculateSimpleRevenue(purchase, _product) {
-   // @TODO: Расчет выручки от операции
+    const { discount, sale_price, quantity } = purchase;
+    // Скидка, преобразованная из процентов в десятичное число
+    const decimalDiscount = discount / 100;
+    // Полная стоимость
+    const totalFullPrice = sale_price * quantity;
+    // Итоговая стоимость (без скидки)
+   const revenue = totalFullPrice * (1 - decimalDiscount);
+
+   return revenue;
 }
 
 /**
@@ -16,7 +24,25 @@ function calculateSimpleRevenue(purchase, _product) {
  * @returns {number}
  */
 function calculateBonusByProfit(index, total, seller) {
-    // @TODO: Расчет бонуса от позиции в рейтинге
+    const { profit } = seller;
+
+    // 15% бонус (первое место по прибыли)
+    if (index === 0) {
+        return profit * 0.15;
+    }
+
+    // 10% бонус (второе и третье места по прибыли)
+    if (index === 1 || index === 2) {
+        return profit * 0.1;
+    }
+
+    // 0% бонус (последнее место по прибыли)
+    if (index === total - 1) {
+        return 0;
+    }
+
+    // 5% бонус (оставшиеся места по прибыли)
+    return profit * 0.05;
 }
 
 /**
